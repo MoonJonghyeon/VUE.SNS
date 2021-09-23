9128b6dd60e20bf8e10f58d647b4b06a2345a1cf
Moontwo <moonjong93@gmail.com>
2021-09-23 17:20:02 +0900

    til dotenv

--- ch1/back/app.js
+++ ch1/back/app.js
@@ -5,7 +5,11 @@ const passport = require('passport')
 const session = require('express-session')
 const cookie = require('cookie-parser')
 const morgan = require('morgan')
+const hpp = require('hpp')
+const helmet = require('helmet')
+const dotenv = require('dotenv')
 
+const prod = process.env.NODE_ENV === 'production',
 const db = require('./models')
 const passportConfig = require('./passport');
 const userRouter = require('./routes/user')
@@ -14,25 +18,39 @@ const postsRouter = require('./routes/posts')
 const hashtagRouter = require('./routes/hashtag')
 const app = express();
 
+dotenv.config();
 db.sequelize.sync();
 passportConfig();
 
-app.use(morgan('dev'))
-app.use(cors({
-    origin: 'http://localhost:3080',
-    credentials: true, 
-}))
+if (prod) {
+    app.user(helmet())
+    app.user(hpp())
+    app.use(morgan('combined'))
+    app.use(cors({
+        origin: 'http://vue.nodebird.com',
+        credentials: true, 
+    }))
+} else {
+    app.use(morgan('dev'))
+    app.use(cors({
+        origin: 'http://localhost:3080',
+        credentials: true, 
+    }))
+}
+
+
 app.use('/', express.static('uploads'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
-app.use(cookie('cookieSecret'))
+app.use(cookie('process.env.COOKIE_SECRET'))
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: 'cookieSecret',
+    secret: 'process.env.COOKIE_SECRET',
     cookie: {
         httpOnly: true,
-        secure: false
+        secure: false,
+        domain: prod && '.nodebird.com',
     }
 }));
 app.use(passport.initialize());
@@ -47,6 +65,6 @@ app.use('/post', postRouter)
 app.use('/posts', postsRouter)
 app.use('/hashtag', hashtagRouter)
 
-app.listen(3085, () => {
-    console.log(`백엔드 서버 ${3085}번 포트에서 작동중`)
+app.listen(prod ? process.env.PORT : 3085, () => {
+    console.log(`백엔드 서버 ${prod ? process.env.PORT : 3085}번 포트에서 작동중`)
 })
