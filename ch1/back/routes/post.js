a2ca195533e2486c243c8023d60773bc2aef67f7
Moontwo <moonjong93@gmail.com>
2021-09-29 16:21:10 +0900

    ket edit to key

--- ch1/back/routes/post.js
+++ ch1/back/routes/post.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 AWS.config.update({
     region: 'ap-northeast-2',
-    accessKetId: process.env.S3_ACCESS_KEY_ID,
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 })
 
@@ -20,8 +20,8 @@ const upload = multer({
     storage: multerS3({
         s3: new AWS.S3(),
         bucket: 'sns-img',
-        ket(req, file, cb) {
-            db(null, `original/${Date.now()}${path.basename(file.originalname)}`)
+        key(req, file, cb) {
+            cb(null, `original/${Date.now()}${path.basename(file.originalname)}`)
         }
     }),
     limit: {fileSize: 20 * 1024 * 1024}
